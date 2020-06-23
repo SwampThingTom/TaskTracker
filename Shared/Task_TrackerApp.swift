5879fde69a5eb6a3e8d458b7e4f9101e92eeff65
Tom Aylesworth <tom.aylesworth@appian.com>
2020-06-23 16:09:59 -0400

    Initial Commit

---
+++ Shared/Task_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Task_TrackerApp.swift
+//  Shared
+//
+//  Created by Tom Aylesworth on 6/23/20.
+//
+
+import SwiftUI
+
+@main
+struct Task_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

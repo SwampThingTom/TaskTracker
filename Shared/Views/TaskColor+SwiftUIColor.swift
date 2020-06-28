bd6db8c17947f23ad46563ea0e9a760ca72468a4
Tom Aylesworth <tom.aylesworth@appian.com>
2020-06-27 23:52:09 -0400

    Reorder colors on color picker

--- Shared/Views/TaskColor+SwiftUIColor.swift
+++ Shared/Views/TaskColor+SwiftUIColor.swift
@@ -11,13 +11,13 @@ extension TaskColor {
     var color: Color {
         switch self {
         case .color1: return Color.blue
-        case .color2: return Color.gray
-        case .color3: return Color.green
+        case .color2: return Color.green
+        case .color3: return Color.yellow
         case .color4: return Color.orange
         case .color5: return Color.pink
-        case .color6: return Color.purple
-        case .color7: return Color.red
-        case .color8: return Color.yellow
+        case .color6: return Color.red
+        case .color7: return Color.purple
+        case .color8: return Color.gray
         }
     }
 }
